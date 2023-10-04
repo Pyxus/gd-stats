35640be1b4b24b4228723f6b35e71499849a0d5a
Ezekiel <ehalle4@students.towson.edu>
2023-10-03 20:22:23 -0400

    Add todo comments

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+/*
+TODO:
+Script Stats:
+	- Report Script Count
+	- Report Lines
+	- Report Empty Lines
+	- Report Characters
+	- Total size
+
+Scene Stats:
+	- Report Scene count
+	- Report Node count
+	- Total size
+
+Shaders:
+	- Report Lines
+	- Report Empty Lines
+	- Report Characters
+	- Total size
+
+Resources:
+	- Report resource count
+	- Total count
+*/
+
+func main() {
+	fmt.Println("Hello")
+}
